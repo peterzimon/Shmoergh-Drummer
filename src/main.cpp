af9f40f70b75a60329632f70466cf1894eb53128
Peter Zimon <peter.zimon@gmail.com>
2021-01-17 00:34:21 +0100

    Added patterns for all channels

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #define CLOCK_PULSE_LENGTH 10       // (ms) Adjust this according to the clock source pulse length
 #define TRIGGER_LENGTH 50           // (ms) How long should a trigger last
 #define PATTERN_SELECTOR_BD A0      // Bass drum pattern selector
+#define PATTERN_SELECTOR_SN A1      // Bass drum pattern selector
+#define PATTERN_SELECTOR_HHC A2     // Bass drum pattern selector
+#define PATTERN_SELECTOR_HHO A3     // Bass drum pattern selector
 
 // Preset pins
 // CLOCK_LED 3             Clock (= clockIn / 4)
@@ -34,6 +37,8 @@ int noOfPatternsHHO = 0;
 int resetButtonState = 0;
 int prevResetState = 0;
 
+// Utility variables
+int patternMux = 0;
 
 // Just blink a pin
 void blink(int pin) {
@@ -100,29 +105,47 @@ void loop() {
         }
 
         // Trigger snare - on pin 5 of PORTD
-        if (seqSN[0][currentStep] == 1) {
+        if (seqSN[patternSN][currentStep] == 1) {
             portDOut = portDOut | B00100000;
         }
 
         // Trigger hihat closed - on pin 6 of PORTD
-        if (seqHHC[0][currentStep] == 1) {
+        if (seqHHC[patternHHC][currentStep] == 1) {
             portDOut = portDOut | B01000000;
         }
 
         // Trigger hihat open - on pin 6 of PORTD
-        if (seqHHO[0][currentStep] == 1) {
+        if (seqHHO[patternHHO][currentStep] == 1) {
             portDOut = portDOut | B10000000;
         }
 
         // Update outputs
         PORTD = portDOut;
+        
+        //Multiplex reading of patterns (analog outputs) so that it doesn't delay triggers
+        // Basically, care only one change at a time and only at 16th notes. This also
+        // avoids sudden changes.
+        patternMux++;
+        switch (patternMux)
+        {
+        case 0:
+            patternBD = getPattern(noOfPatternsBD, analogRead(PATTERN_SELECTOR_BD));
+            break;
+        case 1:
+            patternSN = getPattern(noOfPatternsSN, analogRead(PATTERN_SELECTOR_SN));
+            break;
+        case 2:
+            patternHHC = getPattern(noOfPatternsHHC, analogRead(PATTERN_SELECTOR_HHC));
+            break;
+        case 3:
+            patternHHO = getPattern(noOfPatternsHHO, analogRead(PATTERN_SELECTOR_HHO));
+            patternMux = 0;
+            break;
+        default:
+            break;
+        }
 
-        // TODO: multiplex reading of analog output so that it doesn't delay triggers
-        // int bdPattern = analogRead(PATTERN_SELECTOR_BD);
-        // Serial.println(bdPattern);
-        patternBD = getPattern(noOfPatternsBD, analogRead(PATTERN_SELECTOR_BD));
-        Serial.print("BD pattern: ");
-        Serial.println(patternBD);
+        Serial.println(patternSN);
         
         // Reset
         currentStep++;
